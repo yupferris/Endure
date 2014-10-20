2e38653e8664f491f11c8a2e0414a4b9d9fe6199
ferris <jake@outracks.com>
2014-10-20 22:34:12 +0200

    moved list implementation into the proper header

--- Tests/ListTests.cpp
+++ Tests/ListTests.cpp
@@ -1,40 +1,8 @@
 #include <Endure.h>
 #include "Catch/single_include/catch.hpp"
 
-#include <memory>
-
 using namespace Endure;
 using namespace std;
-
-namespace Endure
-{
-	template <typename T> class _List;
-	template <typename T> class _List
-	{
-	public:
-		static shared_ptr<_List<T>> Cons(T item, shared_ptr<_List<T>> list)
-		{
-			return shared_ptr<_List<T>>(new _List<T>(item, list));
-		}
-
-		const T Head;
-		const shared_ptr<_List<T>> Tail;
-		const int Count;
-
-		_List(T head)
-			: Head(head), Tail(nullptr), Count(1)
-		{
-		}
-
-		_List(T head, shared_ptr<_List<T>> tail)
-			: Head(head), Tail(tail), Count(tail.get()->Count + 1)
-		{
-		}
-	};
-
-	// TODO: Upgrade VS and use this everywhere
-	//template <typename T> using List = shared_ptr<_List<T>>;
-}
 
 TEST_CASE("Create single element int list", "[List]")
 {
--- include/Endure/List.h
+++ include/Endure/List.h
@@ -1,8 +1,36 @@
 #ifndef __ENDURE_LIST_H__
 #define __ENDURE_LIST_H__
 
+#include <memory>
+
 namespace Endure
 {
+	template <typename T> class _List;
+	template <typename T> class _List
+	{
+	public:
+		static std::shared_ptr<_List<T>> Cons(T item, std::shared_ptr<_List<T>> list)
+		{
+			return std::shared_ptr<_List<T>>(new _List<T>(item, list));
+		}
+
+		const T Head;
+		const std::shared_ptr<_List<T>> Tail;
+		const int Count;
+
+		_List(T head)
+			: Head(head), Tail(nullptr), Count(1)
+		{
+		}
+
+		_List(T head, std::shared_ptr<_List<T>> tail)
+			: Head(head), Tail(tail), Count(tail.get()->Count + 1)
+		{
+		}
+	};
+
+	// TODO: Upgrade VS and use this everywhere
+	//template <typename T> using List = shared_ptr<_List<T>>;
 }
 
 #endif
