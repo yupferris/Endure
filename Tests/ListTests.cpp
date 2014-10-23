42a6f43178f9770a9357c992afacc56e002dbef5
ferris <jake@outracks.com>
2014-10-23 15:55:30 +0200

    added empty list semantics; this feels much better now

--- Tests/ListTests.cpp
+++ Tests/ListTests.cpp
@@ -3,13 +3,22 @@
 
 using namespace Endure;
 
+TEST_CASE("Create empty float list", "[List]")
+{
+	auto l = CreateList<float>();
+
+	REQUIRE_THROWS_AS(l->Head(), std::logic_error);
+	REQUIRE_THROWS_AS(l->Tail(), std::logic_error);
+	REQUIRE(!l->Count());
+}
+
 TEST_CASE("Create single element int list", "[List]")
 {
-	auto l = Cons(6);
+	auto l = CreateList(6);
 
-	REQUIRE(l->Head == 6);
-	REQUIRE(!l->Tail);
-	REQUIRE(l->Count == 1);
+	REQUIRE(l->Head() == 6);
+	REQUIRE(!l->Tail()->Count());
+	REQUIRE(l->Count() == 1);
 }
 
 TEST_CASE("Create two element int list", "[List]")
@@ -17,37 +26,37 @@ TEST_CASE("Create two element int list", "[List]")
 	auto tail = CreateList(29);
 	auto l = Cons(45, tail);
 
-	REQUIRE(l->Head == 45);
-	REQUIRE(l->Tail == tail);
-	REQUIRE(l->Count == 2);
+	REQUIRE(l->Head() == 45);
+	REQUIRE(l->Tail() == tail);
+	REQUIRE(l->Count() == 2);
 
-	List<int> t = l->Tail;
-	REQUIRE(t->Head == 29);
-	REQUIRE(!t->Tail);
-	REQUIRE(t->Count == 1);
+	List<int> t = l->Tail();
+	REQUIRE(t->Head() == 29);
+	REQUIRE(!t->Tail()->Count());
+	REQUIRE(t->Count() == 1);
 }
 
 TEST_CASE("Cons to single element int list", "[List]")
 {
-	auto l = Cons(2000);
+	auto l = CreateList(2000);
 	auto l2 = Cons(1984, l);
 
-	REQUIRE(l2->Head == 1984);
-	REQUIRE(l2->Tail == l);
-	REQUIRE(l2->Count == 2);
+	REQUIRE(l2->Head() == 1984);
+	REQUIRE(l2->Tail() == l);
+	REQUIRE(l2->Count() == 2);
 
-	REQUIRE(l2->Tail->Head == 2000);
-	REQUIRE(!l2->Tail->Tail);
-	REQUIRE(l2->Tail->Count == 1);
+	REQUIRE(l2->Tail()->Head() == 2000);
+	REQUIRE(!l2->Tail()->Tail()->Count());
+	REQUIRE(l2->Tail()->Count() == 1);
 }
 
-TEST_CASE("Simple int list cons stress test", "[List")
+TEST_CASE("Simple int list cons stress test", "[List]")
 {
 	auto l = CreateList(0);
 	for (int i = 1; i < 10000; i++)
 		l = Cons(i, l);
 
-	REQUIRE(l->Head == 9999);
-	REQUIRE(l->Tail->Count == 9999);
-	REQUIRE(l->Count == 10000);
+	REQUIRE(l->Head() == 9999);
+	REQUIRE(l->Tail()->Count() == 9999);
+	REQUIRE(l->Count() == 10000);
 }
--- include/Endure/List.h
+++ include/Endure/List.h
@@ -1,6 +1,7 @@
 #ifndef __ENDURE_LIST_H__
 #define __ENDURE_LIST_H__
 
+#include <exception>
 #include <memory>
 
 namespace Endure
@@ -9,36 +10,91 @@ namespace Endure
 	template <typename T> class _List
 	{
 	public:
-		const T Head;
-		const std::shared_ptr<_List<T>> Tail;
-		const int Count;
-
-		_List(T head)
-			: Head(head), Tail(nullptr), Count(1)
+		virtual ~_List()
 		{
 		}
 
-		_List(T head, std::shared_ptr<_List<T>> tail)
-			: Head(head), Tail(tail), Count(tail.get()->Count + 1)
+		virtual T Head() const = 0;
+		virtual std::shared_ptr<_List<T>> Tail() const = 0;
+		virtual int Count() const = 0;
+	};
+
+	template <typename T> class _EmptyList : public _List<T>
+	{
+	public:
+		virtual ~_EmptyList()
 		{
 		}
+
+		virtual T Head() const
+		{
+			throw std::logic_error("Cannot get Head of an empty list");
+		}
+
+		virtual std::shared_ptr<_List<T>> Tail() const
+		{
+			throw std::logic_error("Cannot get Tail of an empty list");
+		}
+
+		virtual int Count() const
+		{
+			return 0;
+		}
+	};
+
+	template <typename T> class _Cons : public _List<T>
+	{
+	public:
+		_Cons(T head)
+			: head(head), tail(std::shared_ptr<_List<T>>(new _EmptyList<T>())), count(1)
+		{
+		}
+
+		_Cons(T head, std::shared_ptr<_List<T>> tail)
+			: head(head), tail(tail), count(tail.get()->Count() + 1)
+		{
+		}
+
+		virtual ~_Cons()
+		{
+		}
+
+		virtual T Head() const
+		{
+			return head;
+		}
+
+		virtual std::shared_ptr<_List<T>> Tail() const
+		{
+			return tail;
+		}
+
+		virtual int Count() const
+		{
+			return count;
+		}
+
+	private:
+		const T head;
+		const std::shared_ptr<_List<T>> tail;
+		const int count;
 	};
 
 	template <typename T> using List = std::shared_ptr<_List<T>>;
 
-	template <typename T> List<T> CreateList(T head)
+	template <typename T> List<T> CreateList()
 	{
-		return std::make_shared<_List<T>>(_List<T>(head));
+		return List<T>(new _EmptyList<T>());
 	}
 
-	template <typename T> List<T> Cons(T head)
+	template <typename T> List<T> CreateList(T head)
 	{
-		return CreateList(head);
+		return List<T>(new _Cons<T>(head));
 	}
 
 	template <typename T> List<T> Cons(T head, List<T> tail)
 	{
-		return std::make_shared<_List<T>>(_List<T>(head, tail));
+		return List<T>(new _Cons<T>(head, tail));
 	}
 }
 
