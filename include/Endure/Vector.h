ef875cd3fdfab2806549bc1ab2d878c36d10e542
ferris <jake@outracks.com>
2014-10-27 23:40:12 +0100

    lots of vector progress, this time by trying to port clojure's actual source instead of figuring it out myself from scratch. Builds now, but crashes pretty hard :)

--- include/Endure/Vector.h
+++ include/Endure/Vector.h
@@ -9,14 +9,36 @@ namespace Endure
 	template <typename T> class _Node
 	{
 	public:
-		T Elements[32];
+		const int Count;
+		T *Elements;
+
+		_Node(int count, T *elements)
+			: Count(count), Elements(elements)
+		{
+		}
+
+		_Node(int count)
+			: Count(count), Elements(new T[count])
+		{
+		}
+
+		_Node()
+			: Count(0), Elements(nullptr)
+		{
+		}
+
+		~_Node()
+		{
+			if (Elements)
+				delete [] Elements;
+		}
 	};
 
 	template <typename T> class _Vector
 	{
 	public:
 		_Vector()
-			: root(nullptr), count(0)
+			: count(0), shift(5), root(new _Node<std::shared_ptr<void>>()), tail(new _Node<T>())
 		{
 		}
 
@@ -25,36 +47,113 @@ namespace Endure
 			return count;
 		}
 
-		std::shared_ptr<_Vector<T>> Conj(T item)
+		std::shared_ptr<_Vector<T>> Conj(T item) const
 		{
-			auto n = new _Node<T>();
-			for (int i = 0; i < count; i++)
-				n->Elements[i] = root->Elements[i];
-			n->Elements[count] = item;
-			return std::shared_ptr<_Vector<T>>(new _Vector<T>(std::shared_ptr<_Node<T>>(n), count + 1));
+			if (count - TailOffset() < 32)
+			{
+				auto tailNode = (_Node<T> *)tail.get();
+				int tailCount = tailNode->Count + 1;
+				auto elements = new T[tailCount];
+				for (int i = 0; i < tailNode->Count; i++)
+					elements[i] = tailNode->Elements[i];
+				elements[tailNode->Count] = item;
+				return std::shared_ptr<_Vector<T>>(new _Vector(count + 1, shift, root, std::shared_ptr<void>(new _Node<T>(tailCount, elements))));
+			}
+
+			std::shared_ptr<void> newRoot;
+			int newShift = shift;
+
+			if ((count >> 5) > (1 << shift))
+			{
+				auto n = new _Node<std::shared_ptr<void>>(32);
+				n->Elements[0] = root;
+				n->Elements[1] = NewPath(shift, tail);
+				newRoot = std::shared_ptr<void>(n);
+				newShift += 5;
+			}
+			else
+			{
+				newRoot = PushTail(shift, (_Node<std::shared_ptr<void>> *)root.get(), tail);
+			}
+
+			auto newTailElements = new T[1];
+			newTailElements[0] = item;
+			return std::shared_ptr<_Vector<T>>(new _Vector(count + 1, newShift, newRoot, std::shared_ptr<void>(new _Node<T>(1, newTailElements))));
 		}
 
-		T Get(int key)
+		T Get(int i) const
 		{
-			if (key < 0 || key >= count)
-				throw std::out_of_range("Key was out of bounds");
-
-			return root->Elements[key];
+			auto node = ArrayFor(i);
+			return node[i & 0x1f];
 		}
 
 	private:
-		_Vector(std::shared_ptr<_Node<T>> root)
-			: root(root), count(1)
+		_Vector(int count, int shift, std::shared_ptr<void> root, std::shared_ptr<void> tail)
+			: count(count), shift(shift), root(root), tail(tail)
 		{
 		}
 
-		_Vector(std::shared_ptr<_Node<T>> root, int count)
-			: root(root), count(count)
+		const T *ArrayFor(int i) const
 		{
+			if (i < 0 || i >= count)
+				throw std::out_of_range("Index was out of bounds");
+
+			if (i >= TailOffset())
+				return ((_Node<T> *)tail.get())->Elements;
+
+			auto node = root.get();
+			for (int level = shift; level > 0; level -= 5)
+				node = ((_Node<std::shared_ptr<void>> *)node)->Elements[(i >> level) & 0x1f].get();
+			return ((_Node<T> *)node)->Elements;
 		}
 
-		const std::shared_ptr<_Node<T>> root;
+		int TailOffset() const
+		{
+			if (count < 32)
+				return 0;
+
+			return ((count - 1) >> 5) << 5;
+		}
+
+		std::shared_ptr<void> PushTail(int level, const _Node<std::shared_ptr<void>> *parent, std::shared_ptr<void> tail) const
+		{
+			int i = ((count - 1) >> level) & 0x1f;
+			auto elements = new std::shared_ptr<void>[parent->Count];
+			for (int i = 0; i < parent->Count; i++)
+				elements[i] = parent->Elements[i];
+			auto ret = new _Node<std::shared_ptr<void>>(parent->Count, elements);
+			std::shared_ptr<void> nodeToInsert;
+
+			if (level == 5)
+			{
+				nodeToInsert = tail;
+			}
+			else
+			{
+				auto child = parent->Elements[i];
+				nodeToInsert = child.get() ?
+					PushTail(level - 5, (_Node<std::shared_ptr<void>> *)child.get(), tail) :
+					NewPath(level - 5, tail);
+			}
+
+			ret->Elements[i] = nodeToInsert;
+			return std::shared_ptr<void>(ret);
+		}
+
+		static std::shared_ptr<void> NewPath(int level, std::shared_ptr<void> node)
+		{
+			if (level == 0)
+				return node;
+
+			auto ret = new _Node<std::shared_ptr<void>>(32);
+			ret->Elements[0] = NewPath(level - 5, node);
+			return std::shared_ptr<void>(ret);
+		}
+
 		const int count;
+		const int shift;
+		const std::shared_ptr<void> root;
+		const std::shared_ptr<void> tail;
 	};
 
 	template <typename T> using Vector = std::shared_ptr<_Vector<T>>;
